3e883d8bf293aaed7c7ad7259d195be3bf1f7b62
Demby Abella <dmcchy@gmail.com>
2019-04-10 16:28:57 +0800

    ++ now able to increment TimeFighter's score

--- app/src/main/java/com/example/timefighter/MainActivity.kt
+++ app/src/main/java/com/example/timefighter/MainActivity.kt
@@ -1,12 +1,61 @@
 package com.example.timefighter
 
-import android.support.v7.app.AppCompatActivity
+// Adding these 4 additional imports for support
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
 
+/**
+ * "Activity" - is the composition of every android app.
+ */
+
+// MainActivity is a subclass of AppCompatActivity().
 class MainActivity : AppCompatActivity() {
 
+    internal lateinit var gameScoreTextView: TextView
+    internal lateinit var timeLeftTextView: TextView
+    internal lateinit var tapMeButton: Button
+
+    internal var score = 0
+
+    // I think this is a lifecycle entry.
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // This is required. Bootstraps your layout and passes it on the Android screen.
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_main)
+
+        // Connect variables to views.
+
+        // Variables
+        gameScoreTextView = findViewById<TextView>(R.id.game_score_text_view)
+        timeLeftTextView = findViewById<TextView>(R.id.time_left_text_view)
+        tapMeButton = findViewById<Button>(R.id.tap_me_button)
+
+        // Listener
+        tapMeButton.setOnClickListener { _ -> incrementScore() }
     }
+
+    private fun incrementScore() {
+        score++
+
+        val newScore = "Your Score: " + Integer.toString(score)
+        gameScoreTextView.text = newScore
+    }
+
+    private fun resetGame() {
+
+    }
+
+    private fun startGame() {
+
+    }
+
+    private fun endGame() {
+
+    }
+
 }
