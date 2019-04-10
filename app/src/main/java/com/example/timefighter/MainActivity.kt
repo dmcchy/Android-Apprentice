136f6903665e68bdcbb8d9b82cbbae99c6432ae9
Demby Abella <dmcchy@gmail.com>
2019-04-10 17:04:35 +0800

    ++ clock now starting when running the game

--- app/src/main/java/com/example/timefighter/MainActivity.kt
+++ app/src/main/java/com/example/timefighter/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.timefighter
 
 // Adding these 4 additional imports for support
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
@@ -12,6 +13,13 @@ import android.widget.TextView
 
 // MainActivity is a subclass of AppCompatActivity().
 class MainActivity : AppCompatActivity() {
+
+    internal var gameStarted = false
+
+    internal lateinit var countDownTimer: CountDownTimer
+    internal var initialCountDown: Long = 60000
+    internal var countDownInterval: Long = 1000
+    internal var timeLeft = 60
 
     internal lateinit var gameScoreTextView: TextView
     internal lateinit var timeLeftTextView: TextView
@@ -37,9 +45,16 @@ class MainActivity : AppCompatActivity() {
 
         // Listener
         tapMeButton.setOnClickListener { _ -> incrementScore() }
+
+        resetGame()
     }
 
     private fun incrementScore() {
+
+        if (!gameStarted){
+            startGame()
+        }
+
         score++
 
         // val newScore = "Your Score: " + Integer.toString(score)
@@ -52,11 +67,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun resetGame() {
+        // 1
+        score = 0
 
+        val initialScore = getString(R.string.your_score, Integer.toString(score))
+
+        gameScoreTextView.text = initialScore
+
+        var initialTimeLeft = getString(R.string.time_left, Integer.toString(60))
+        timeLeftTextView.text = initialTimeLeft
+
+        // 2
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
+            // 3
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeft = millisUntilFinished.toInt() / 1000
+
+                var timeLeftString = getString(R.string.time_left, Integer.toString(timeLeft))
+                timeLeftTextView.text = timeLeftString
+            }
+
+            override fun onFinish() {
+                // To be implemented later
+            }
+        }
+
+        // 4
+        gameStarted = false
     }
 
     private fun startGame() {
-
+        countDownTimer.start()
+        gameStarted = true
     }
 
     private fun endGame() {
